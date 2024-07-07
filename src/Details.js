0cab4e475de0eb349078b58549a57e9e20ba1fb1
vivekvag <vivekvag281@gmail.com>
2024-07-07 14:06:35 +0530

    fix: updated http to https

--- src/Details.js
+++ src/Details.js
@@ -10,7 +10,7 @@ class Details extends Component {
 
   async componentDidMount() {
     const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+      `https://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await res.json();
     this.setState(
